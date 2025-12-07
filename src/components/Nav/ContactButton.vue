438dca65ee6ec9453466bfcd678e837f51c68c40
Danny <dannyjag@gmail.com>
2025-12-07 19:26:56 +0000

    Added some styling for the Contact 'button' and the menu items, desktop

--- src/components/Nav/ContactButton.vue
+++ src/components/Nav/ContactButton.vue
@@ -1,5 +1,17 @@
 <template>
-    <a :href="item.link" class="border py-1 text-lg font-medium px-3 rounded-2xl inline-block text-center">
+    <a :href="item.link" class="
+      inline-block
+      px-6 py-2
+      text-lg font-medium
+      rounded-2xl
+      border border-black
+      shadow-[4px_4px_0_0_rgba(0,0,0,1)]
+      transform
+      transition
+      hover:-translate-y-1 hover:shadow-[6px_6px_0_0_rgba(0,0,0,1)]
+      active:translate-y-0 active:shadow-[4px_4px_0_0_rgba(0,0,0,1)]
+      text-center bg-medium-purple
+    ">
       {{ item.title }}
     </a>
 </template>
--- src/components/Nav/DesktopMenu.vue
+++ src/components/Nav/DesktopMenu.vue
@@ -1,5 +1,5 @@
 <template>
-    <ul class="flex gap-4 bg-medium-purple/80 py-2 text-lg lg:text-xl font-medium px-5 md:px-10 rounded-2xl">
+    <ul class="flex gap-4 bg-medium-purple/80 py-2 border-2 border-medium-purple text-lg lg:text-xl font-medium px-5 md:px-10 rounded-2xl">
         <li v-for="item in items" :key="item.title">
             <a :href="item.link"  class="relative after:absolute after:-bottom-0.5 after:left-1/2 after:h-0.5 after:w-0 after:bg-white after:transition-all after:duration-150 hover:after:-translate-x-1/2 hover:after:w-full">{{ item.title }} </a>
         </li>
