e245af60f49702266c1c8761e772d63dc6b1d5ed
Danny <dannyjag@gmail.com>
2025-12-08 23:47:17 +0000

    ...

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import SkillsSection from './components/SkillsSection.vue';
       <NavBar />
       <div class="relative flex h-[calc(100dvh-80px)]">
         <div
-          class="absolute inset-0 z-0 bg-gradient-to-b from-black/50 via-black/20 to-black/50"
+          class="absolute inset-0 z-0 bg-linear-to-b from-black/50 via-black/20 to-black/50"
         ></div>
         <HomeSection class="relative z-10" />
       </div>
