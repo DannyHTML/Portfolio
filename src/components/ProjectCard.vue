daa6dcecf24c57f3d040e94787ea061d7e6dd728
Danny <dannyjag@gmail.com>
2025-12-09 12:23:14 +0000

    Removed coded that I tested with and removed a TODO

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -3,13 +3,11 @@
     <div class="bg-medium-purple rounded-xl border border-gray-500 p-3">
       <h3 class="mb-4 text-center text-xl font-bold">{{ project.title }}</h3>
       <div>
-        <!-- TODO: Could do either an iframe or a screenshot -->
         <img
           :src="project.link"
           :alt="`Screenshot of ${project.title}`"
           class="w-full rounded-lg object-cover"
         />
-        <!-- <iframe :src="project.link" class="h-96 w-full rounded-lg border-4 bg-white"></iframe> -->
       </div>
       <p class="bg-quaternary/30 mt-3 rounded-md border border-gray-500 p-2 font-medium">
         {{ project.description }}
