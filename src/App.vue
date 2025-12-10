d3d05c70a3425d80848e4fb07db144a1702ed0ac
Danny <dannyjag@gmail.com>
2025-12-10 12:33:29 +0000

    Added TODO

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ import ProjectSection from './components/ProjectSection.vue';
   <!-- TODO: I think I keep this layout? -->
   <!-- TODO: Need to implement a button up when you start scrolling. -->
   <!-- TODO: Add meta description and keywords plus other SEO relevant tags like image in index.html -->
+  <!-- TODO: A pop up to show all skills while you press a button? -->
   <main>
     <div class="backgroundImage relative min-h-screen">
       <NavBar />
