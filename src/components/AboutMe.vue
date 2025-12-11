a10a5a8d26dbd7aca730b8b56eed30ec2996bbf1
Danny <dannyjag@gmail.com>
2025-12-11 22:20:19 +0000

    Added the links for the websites to showcase. Added TODO for resuable component. Changed styling

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -59,8 +59,14 @@
           <p>
             I’m a front-end developer with a strong focus on building clean, scalable, and
             user-friendly web experiences. Originally self-taught, I later completed a
-            government-funded Front-End Development program in the Netherlands through Hacklab.frl,
-            which gave me a solid foundation and helped refine my development approach. I’ve been
+            government-funded Front-End Development program in the Netherlands through
+            <a
+              href="https://hacklab.frl/stacks/front-end-development"
+              class="text-blue-500 underline"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Hacklab.frl</a
+            >, which gave me a solid foundation and helped refine my development approach. I’ve been
             living in the UK since October 2025, where I continue to grow both personally and
             professionally. <br /><br />
             After completing my training, I joined Fotocadeau B.V. as a Front-End Developer.
@@ -71,37 +77,69 @@
             multilingual e-commerce websites built on Magento 2 and powered by PrismicIO as a
             headless CMS. My responsibilities included translating Figma designs into fully
             responsive interfaces, collaborating with marketing to evaluate feasibility and
-            timelines, and proposing my own ideas to improve usability and performance. <br /><br />
-            A large part of my work involved building reusable Prismic slices and custom types so
-            teams like Marketing and Sales could manage content efficiently without developer
-            involvement. I also worked on the integration layer between Magento 2 and PrismicIO
-            using PHTML, CSS/Tailwind, XML, and Alpine.js, and implemented front-end features in
-            both Magento 1 and Magento 2 when appropriate. <br /><br />
-            Some examples of my work include: <br /><br />
+            timelines, and proposing my own ideas to improve usability and performance.
           </p>
-          <ul>
-            <li class="list-inside list-disc">
-              Topdoek.nl – Created various reusable slices in Prismic that allowed Marketing to
-              control design and feature toggles.
-            </li>
-            <li class="list-inside list-disc">
-              NoBoringSuitcases.de / NoBoringSuitcases.nl – Developed custom Prismic types for the
-              Sales team, turning their design and content requirements into flexible, easy-to-use
-              components.
-            </li>
-          </ul>
-          <br />
-          <p>
-            Topdoek.nl – Created various reusable slices in Prismic that allowed Marketing to
-            control design and feature toggles. NoBoringSuitcases.de / NoBoringSuitcases.nl –
-            Developed custom Prismic types for the Sales team, turning their design and content
-            requirements into flexible, easy-to-use components.
-            <br /><br />
-            I enjoy transforming ideas into functional, maintainable interfaces, collaborating
-            across teams, and building systems that are as enjoyable to work with as they are to
-            use.
-          </p>
+          <div class="mt-2 flex justify-end">
+            <!-- TODO: Make this a re-useable button component, also for in the header/nav and mobile menu -->
+            <a
+              class="bg-medium-purple inline-block transform cursor-pointer rounded-2xl border border-black px-6 py-2 text-lg font-medium shadow-[4px_4px_0_0_rgba(0,0,0,1)] transition hover:-translate-y-1 hover:shadow-[6px_6px_0_0_rgba(0,0,0,1)] active:translate-y-0 active:shadow-[4px_4px_0_0_rgba(0,0,0,1)]"
+            >
+              Download CV </a
+            >``
+          </div>
         </div>
+      </div>
+      <div class="mt-5 lg:mt-14">
+        <p>
+          A large part of my work involved building reusable Prismic slices and custom types so
+          teams like Marketing and Sales could manage content efficiently without developer
+          involvement. I also worked on the integration layer between Magento 2 and PrismicIO using
+          PHTML, CSS/Tailwind, XML, and Alpine.js, and implemented front-end features in both
+          Magento 1 and Magento 2 when appropriate. <br /><br />
+          Some examples of my work include: <br /><br />
+        </p>
+        <ul>
+          <li class="list-inside list-disc">
+            <a
+              href="https://www.topdoek.nl/"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="text-blue-500 underline"
+              >Topdoek.nl</a
+            >
+            – Created various reusable slices in Prismic that allowed Marketing to control design
+            and feature toggles.
+          </li>
+          <li class="list-inside list-disc">
+            <a
+              href="https://www.noboringsuitcases.de/geschaftlich-nbs"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="text-blue-500 underline"
+              >NoBoringSuitcases.de</a
+            >
+            /
+            <a
+              href="https://www.noboringsuitcases.nl/zakelijk-nbs"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="text-blue-500 underline"
+              >NoBoringSuitcases.nl</a
+            >
+            – Developed custom Prismic types for the Sales team, turning their design and content
+            requirements into flexible, easy-to-use components.
+          </li>
+        </ul>
+        <br />
+        <p>
+          Topdoek.nl – Created various reusable slices in Prismic that allowed Marketing to control
+          design and feature toggles. NoBoringSuitcases.de / NoBoringSuitcases.nl – Developed custom
+          Prismic types for the Sales team, turning their design and content requirements into
+          flexible, easy-to-use components.
+          <br /><br />
+          I enjoy transforming ideas into functional, maintainable interfaces, collaborating across
+          teams, and building systems that are as enjoyable to work with as they are to use.
+        </p>
       </div>
     </div>
   </div>
